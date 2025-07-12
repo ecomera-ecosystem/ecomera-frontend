03a9aa278c537bcf1472be21b029f37f36a82c11
Youssef Ammari <100441472+JoeBroski@users.noreply.github.com>
2025-07-13 00:06:16 +0100

    feat: clean and modern login form UI

---
+++ src/app/features/auth/pages/login/login.component.ts
@@ -0,0 +1,90 @@
+import { Input, Component, Output, EventEmitter } from '@angular/core';
+import {
+  FormsModule,
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  FormControlName,
+  Validators,
+  FormBuilder,
+} from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { Router } from '@angular/router';
+
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
+import { MatCardModule } from '@angular/material/card';
+import { AuthService } from '../../../../core/services/auth.service';
+
+@Component({
+  selector: 'app-login',
+  imports: [
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    FormsModule,
+    CommonModule,
+    MatIconModule,
+    MatCardModule,
+    ReactiveFormsModule,
+  ],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.css',
+  providers: [AuthService],
+})
+export class LoginComponent {
+  debugger: any;
+  loginForm: FormGroup;
+
+  constructor(
+    private router: Router,
+    private authService: AuthService,
+    private fb: FormBuilder
+  ) {
+    this.loginForm = this.fb.group({
+      email: new FormControl('', [Validators.required, Validators.email]),
+      password: new FormControl('', [
+        Validators.required,
+        Validators.minLength(8),
+      ]),
+    });
+  }
+
+  @Input() error: string | null = null;
+
+  // @Output() submitEM = new EventEmitter();
+
+  submit() {
+    if (this.loginForm.valid) {
+      this.authService.login(this.loginForm.value).subscribe({
+        next: (response) => {
+          this.authService.setToken(response.access_token);
+          // console.log('response.access_token', response.access_token);
+          // console.log('response.refresh_token', response.refresh_token);
+          this.router.navigate(['/home']);
+        },
+        error: (err) => {
+          this.error = err.error?.message || 'Login failed';
+          console.error('Login error:', err);
+        },
+        complete: () => {
+          // Optionally handle completion
+          console.log('Login request completed');
+        },
+      });
+    } else {
+      // Mark all fields as touched to show validation errors
+      this.loginForm.markAllAsTouched();
+    }
+  }
+
+  get email() {
+    return this.loginForm.get('email');
+  }
+
+  get password() {
+    return this.loginForm.get('password');
+  }
+}
