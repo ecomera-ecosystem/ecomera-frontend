e6a165b59074ac0fca3cc985c2edd7fd27dbf19e
Youssef Ammari <100441472+JoeBroski@users.noreply.github.com>
2025-07-13 12:13:07 +0100

    feat(register): add auth forms routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,23 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './features/auth/pages/login/login.component';
+import { RegisterComponent } from './features/auth/pages/register/register.component';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'login',
+    title: 'Ecomera',
+  },
+  {
+    path: 'login',
+    component: LoginComponent,
+    title: 'Log in',
+  },
+  {
+    path: 'register',
+    component: RegisterComponent,
+    title: 'Sign up',
+  },
+];
